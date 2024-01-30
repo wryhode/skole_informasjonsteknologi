28276b3e8275f63bbe846bafeff5f413a04f4c95
wryhode <wryhode@gmail.com>
2024-01-30 13:31:10 +0100

    made the typebomt rewrite work still missing features but its playable at least

--- kapittel5/typeBomb/script.js
+++ kapittel5/typeBomb/script.js
@@ -1,3 +1,4 @@
+// Getting document elements
 const mainGameDiv = document.getElementById("mainGameBox");
 const notificationBox = document.getElementById("notificationBox");
 const titleDiv = document.getElementById("titleDiv");
@@ -7,26 +8,124 @@ const gameMessageDiv = document.getElementById("gameMessageDiv");
 const gameMessage = document.getElementById("gameMessage");
 const lettersDiv = document.getElementById("letters");
 
+const charset = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let availableCharset = charset.slice(0);
+let charItersWithoutRefresh = 0;
 let symbols = new Array();
-
+let activeSymbol = null;
 let isNotifying = false;
 let isGameMessageActive = false;
+let takeGameInput = false;
+let loseTimeoutID = null
+let score = 0;
+let timeOutTime = 5000;
 
 class Symbol
 {
     constructor(parentElement, symbol)
     {
         this.parentElement = parentElement;
+        this.element = null
         this.symbol = symbol;
 
         this.createElement = function()
         {
-            let tempElement = document.createElement("div");
-            tempElement.className = "letter";
-            tempElement.innerHTML = this.symbol;
-            this.parentElement.appendChild(tempElement);
+            this.element = document.createElement("div");
+            this.element.className = "letter";
+            this.element.innerHTML = this.symbol;
+            this.parentElement.appendChild(this.element);
+        }
+
+        this.setAnimation = function(animate)
+        {
+            if(animate)
+            {
+                this.element.classList.add("blinkingLetter");
+            }
+            else
+            {
+                this.element.classList.remove("blinkingLetter");
+            }
         }
     }
+}
+
+function handleKeyPress(event)
+{
+    const key = event.key;
+    const gameKey = key.toUpperCase();
+
+    if(takeGameInput)
+    {
+        if(charset.includes(gameKey))
+        {
+            if(gameKey == activeSymbol.symbol)
+            {
+                setupGameChars(5);
+                score ++;
+            }
+            else
+            {
+                misTyped();
+            }
+        }
+    }
+    else
+    {
+        startGame();
+    }
+}
+
+function misTyped()
+{
+    takeGameInput = false;
+    activeSymbol.setAnimation(false);
+    lettersDiv.style.display = "none";
+    displayGameMessage("Game over!");
+    startMenu();
+}
+
+function resetAvailableChars()
+{
+    availableCharset = charset.slice(0);
+}
+
+function pickRandomChar()
+{
+    let randomIndex = Math.floor(Math.random() * availableCharset.length);
+    let char = availableCharset[randomIndex];
+    availableCharset.splice(randomIndex, 1);
+    return char;
+}
+
+function pickRandomCharMulti(nChars)
+{
+    let pickedChars = new Array();
+    for(let i = 0; i < nChars; i ++)
+    {
+        pickedChars.push(pickRandomChar());
+    }
+    return pickedChars;
+}
+
+function hideNotification()
+{
+    isNotifying = false;
+    notificationBox.style.display = boolToDisplayStyle(isNotifying);
+}
+
+function displayNotification(title, message)
+{
+    notificationBox.innerHTML = "";
+    let titleElement = document.createElement("h1");
+    titleElement.innerHTML = title;
+    let contentElement = document.createElement("p");
+    contentElement.innerHTML = message;
+    notificationBox.appendChild(titleElement);
+    notificationBox.appendChild(contentElement);
+    isNotifying = true;
+    notificationBox.style.display = boolToDisplayStyle(isNotifying);
+    notificationBox.addEventListener("animationend", hideNotification)
 }
 
 function hideGameMessage()
@@ -43,18 +142,18 @@ function displayGameMessage(message, fadeTime = 5000)
     gameMessageDiv.addEventListener("animationend", hideGameMessage);
 }
 
-function createSymbol()
+function createSymbol(symbol = "A")
 {
-    let symbol = new Symbol(lettersDiv, "A");
-    symbol.createElement();
-    symbols.push(symbol);
+    let symbolEl = new Symbol(lettersDiv, symbol);
+    symbolEl.createElement();
+    symbols.push(symbolEl);
 }
 
-function createSymbols(nSymbols)
+function createSymbols(nSymbols, symbols)
 {
     for (let i = 0; i < nSymbols; i++)
     {
-        createSymbol();
+        createSymbol(symbols[i]);
     }
 }
 
@@ -70,33 +169,68 @@ function boolToDisplayStyle(show)
     }
 }
 
+function setupGameChars(nChars)
+{
+    clearTimeout(loseTimeoutID);
+
+    symbols = new Array();
+    lettersDiv.innerHTML = "";
+    let charList = pickRandomCharMulti(nChars);
+
+    createSymbols(nChars, charList);  
+
+    selectRandomChar();
+
+    charItersWithoutRefresh ++;
+    if(charItersWithoutRefresh >= 2)
+    {
+        resetAvailableChars();
+        charItersWithoutRefresh = 0;
+    }
+
+    timeOutTime = 5000 / ((score/25) + 1);
+    loseTimeoutID = setTimeout(misTyped, timeOutTime);
+}
+
+function selectRandomChar()
+{
+    let whichChar = Math.floor(Math.random() * symbols.length);
+    activeSymbol = symbols[whichChar];
+    
+    activeSymbol.setAnimation(true);
+}
+
 function showMenuElements(show)
 {
     let displayStyle = boolToDisplayStyle(show);
     scoreMenu.style.display = displayStyle;
     leaderboard.style.display = displayStyle;
-    titleDiv.style.display = displayStyle;
     notificationBox.style.display = boolToDisplayStyle(show * isNotifying);
     gameMessageDiv.style.display = boolToDisplayStyle(show * isGameMessageActive);
 }
 
 function startMenu()
 {
+    takeGameInput = false;
     showMenuElements(true);
+    displayNotification("Tips", "You can restart immediately after a game over by pressing a key");
 }
 
 function startGame()
 {
-    symbols = new Array();
-    createSymbols(5);
+    setupGameChars(5);
     showMenuElements(false);
+    lettersDiv.style.display = "flex";
+    takeGameInput = true;
 }
 
 function init()
 {
+    titleDiv.style.display = "none";
     startMenu();
-    displayGameMessage("gameMessage test");
 }
+
+document.addEventListener("keydown", handleKeyPress);
 
 showMenuElements(false);
 titleDiv.style.display = "unset";
